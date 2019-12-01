d7c6d59dcfc8f66609ffebd526c35bc07431ce27
Joe Moore <jpvlsmv@gmail.com>
2019-12-01 15:45:01 -0500

    Switch to java

--- app/src/main/java/org/iegrec/utilitybelt/UtilityBeltActivity.java
+++ app/src/main/java/org/iegrec/utilitybelt/UtilityBeltActivity.java
@@ -16,10 +16,10 @@ public class UtilityBeltActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.utilitybelt_mainlayout);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
