0e915f8b797c102926a52be129a314e9b9a6b284
Joe Moore <jpvlsmv@gmail.com>
2020-01-05 08:35:24 -0500

    Kick off our Tracer service at boot

--- app/src/main/java/org/iegrec/utilitybelt/UtilityBeltBootReceiver.java
+++ app/src/main/java/org/iegrec/utilitybelt/UtilityBeltBootReceiver.java
@@ -7,6 +7,9 @@ import android.content.Intent;
 public class UtilityBeltBootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        
+        if (intent.getAction().equalsIgnoreCase(Intent.ACTION_BOOT_COMPLETED))  {
+            Intent serviceIntent = new Intent(context, org.iegrec.utilitybelt.UtilityBeltTracer.class);
+            context.startService(serviceIntent);
+        }
     }
 }
