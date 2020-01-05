f4daf875d361300c87b7ed070d752db987bb2b2d
Joe Moore <jpvlsmv@gmail.com>
2020-01-05 08:34:14 -0500

    Set up manifest for start-on-boot service

---
+++ app/src/main/java/org/iegrec/utilitybelt/UtilityBeltBootReceiver.java
@@ -0,0 +1,12 @@
+package org.iegrec.utilitybelt;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class UtilityBeltBootReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        
+    }
+}
