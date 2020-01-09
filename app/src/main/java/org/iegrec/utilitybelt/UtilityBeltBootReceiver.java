49aa99e979780bfc278d0bb400873b182d9b6c1d
Joe Moore <jpvlsmv@gmail.com>
2020-01-08 20:00:59 -0500

    Receive boot complete intent

--- app/src/main/java/org/iegrec/utilitybelt/UtilityBeltBootReceiver.java
+++ app/src/main/java/org/iegrec/utilitybelt/UtilityBeltBootReceiver.java
@@ -4,10 +4,13 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import java.util.Objects;
+
 public class UtilityBeltBootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (intent.getAction().equalsIgnoreCase(Intent.ACTION_BOOT_COMPLETED))  {
+        String s = intent.getAction();
+        if (Objects.requireNonNull(s).equalsIgnoreCase(Intent.ACTION_BOOT_COMPLETED))  {
             Intent serviceIntent = new Intent(context, org.iegrec.utilitybelt.UtilityBeltTracer.class);
             context.startService(serviceIntent);
         }
--- app/src/main/java/org/iegrec/utilitybelt/UtilityBeltTracer.java
+++ app/src/main/java/org/iegrec/utilitybelt/UtilityBeltTracer.java
@@ -2,22 +2,38 @@ package org.iegrec.utilitybelt;
 
 import android.app.Service;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.IBinder;
+import android.preference.PreferenceManager;
+
+import java.time.Instant;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class UtilityBeltTracer extends Service {
-    public UtilityBeltTracer() {
+    private void startTimer(){
+        final Timer timer;
+        timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                SharedPreferences p = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+                SharedPreferences.Editor edit = p.edit();
+                    edit.putString("LASTSEEN", String.valueOf(Instant.now().getEpochSecond()));
+                edit.apply();
+            }
+        }
+        ,60,60);
     }
 
     @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        // TODO: Set things up
-        return Service.START_NOT_STICKY;
+    public void onCreate() {
+        startTimer();
+        super.onCreate();
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
         return null;
-        // throw new UnsupportedOperationException("Not yet implemented");
     }
 }
